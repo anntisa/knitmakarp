8333185ba01de3a47e5084d1dbddbf0568668179
anntisa <annietsai92@gmail.com>
2019-09-09 17:51:20 -0400

    add nav bar and social media links

---
+++ src/main/HeaderNav.js
@@ -0,0 +1,37 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEtsy, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons'
+
+import React from "react";
+
+function HeaderNav() {
+
+    return (
+
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">KnitMaKarp</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+
+                <Nav className="mr-auto">
+                    <Nav.Link href="#item">Items</Nav.Link>
+                </Nav>
+                <Nav>
+                    <Nav.Link href="https://www.etsy.com/shop/knitmakarp" target="_blank">
+                        <FontAwesomeIcon icon={faEtsy} />
+                    </Nav.Link>
+                    <Nav.Link href="https://www.instagram.com/knitmakarps/" target="_blank">
+                        <FontAwesomeIcon icon={faInstagram} />
+                    </Nav.Link>
+                    <Nav.Link href="https://www.facebook.com/knitmakarp/" target="_blank">
+                        <FontAwesomeIcon icon={faFacebook} />
+                    </Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+
+    );
+}
+
+export default HeaderNav;
