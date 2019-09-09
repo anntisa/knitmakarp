3d439118568ce331a78724b00f892042f016a623
anntisa <annietsai92@gmail.com>
2019-09-09 17:51:20 -0400

    add nav bar and social media links

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,21 +1,18 @@
 import React from "react";
-// import Avatar from "@material-ui/core/Avatar";
-// import Typography from "@material-ui/core/Typography";
-// import Container from "@material-ui/core/Container";
-// import logo from './../logo.svg';
+import HeaderNav from "./HeaderNav";
 import './Main.css';
 
-const LogInPage: React.FC = () => {
+function Main() {
     return (
-        <div class="agileits_main">
-            <div class="banner_text_w3layouts">
+        <div className="agileits_main">
+            <HeaderNav />
+            <div className="banner_text_w3layouts">
                 <h3>KnitMaKarp</h3>
                 <span> </span>
                 <p>Personally Hand Crafted and Designed</p>
             </div>
-
         </div>
     );
 };
 
-export default LogInPage;
+export default Main;
